80c375dcd2f9680dd559e2dd58056c21850ffaf5
djc200483 <djc200483@gmail.com>
2025-06-26 14:24:09 +0100

    New Nav Bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './NavBar.css';
 
 export default function NavBar() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const closeTimeout = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (closeTimeout.current) clearTimeout(closeTimeout.current);
+    setDropdownOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    closeTimeout.current = setTimeout(() => setDropdownOpen(false), 120);
+  };
 
   return (
     <nav className="navbar">
       <div className="navbar-content">
         <div
           className="navbar-item navbar-dropdown"
-          onMouseEnter={() => setDropdownOpen(true)}
-          onMouseLeave={() => setDropdownOpen(false)}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
         >
           <button className="navbar-link">Product ▾</button>
           {dropdownOpen && (
-            <div className="navbar-dropdown-menu">
+            <div
+              className="navbar-dropdown-menu"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
               <button className="navbar-dropdown-item">Visual Tools</button>
               <button className="navbar-dropdown-item">Written Tools</button>
             </div>

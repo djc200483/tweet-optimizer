75b4a54697a2e869277b6b09be939e8ea7e5a677
djc200483 <djc200483@gmail.com>
2025-06-26 19:21:02 +0100

    New Nav Bar

--- src/components/WritingToolsPage.js
+++ src/components/WritingToolsPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useAuth } from './auth/AuthContext';
 import Home from './Home';
+import NavBar from './NavBar';
 import './NavBar.css';
 
 export default function WritingToolsPage() {
@@ -9,19 +10,27 @@ export default function WritingToolsPage() {
 
   if (!user || !token) {
     return (
-      <div className="navbar-modal-overlay">
-        <div className="navbar-modal">
-          <div className="navbar-modal-header">
-            <span className="navbar-modal-title">Login Required</span>
-            <button className="navbar-modal-close" onClick={() => setShowLoginModal(false)}>&times;</button>
-          </div>
-          <div className="navbar-modal-body">
-            <p>You must be logged in to access this area. Please log in or register to continue.</p>
+      <>
+        <NavBar />
+        <div className="navbar-modal-overlay">
+          <div className="navbar-modal">
+            <div className="navbar-modal-header">
+              <span className="navbar-modal-title">Login Required</span>
+              <button className="navbar-modal-close" onClick={() => setShowLoginModal(false)}>&times;</button>
+            </div>
+            <div className="navbar-modal-body">
+              <p>You must be logged in to access this area. Please log in or register to continue.</p>
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 
-  return <Home isLoggedIn={true} onSelectFeature={() => {}} initialTab="written" />;
+  return (
+    <>
+      <NavBar />
+      <Home isLoggedIn={true} onSelectFeature={() => {}} initialTab="written" />
+    </>
+  );
 } 
